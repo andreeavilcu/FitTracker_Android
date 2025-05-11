f044c663ce622a621faddee181a58b1e71b8dd49
andreeavilcu <andreea.vilcu@student.unitbv.ro>
2025-05-11 21:29:25 +0300

    fix: Implement Navigation SafeArgs and fix fragment navigation

--- app/src/main/java/com/example/fittracker_android/MainActivity.kt
+++ app/src/main/java/com/example/fittracker_android/MainActivity.kt
@@ -3,18 +3,48 @@ package com.example.fittracker_android
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.fittracker_android.R
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val navInflater = navController.navInflater
+        val navGraph = navInflater.inflate(R.navigation.auth_nav_graph)
+        navController.graph = navGraph
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+
+    }
+
+
+    override fun onBackPressed() {
+        val currentDestination = navController.currentDestination?.id
+        when (currentDestination) {
+            R.id.loginFragment -> {
+                finish()
+            }
+            R.id.registerStep1Fragment, R.id.registerStep2Fragment ->{
+                navController.navigate(R.id.loginFragment)
+            }
+            R.id.profileFragment -> {
+                finish()
+            }
+            else -> {
+                super.onBackPressed()
+            }
         }
     }
 }
