bbe1a166681983538a5d5abfbe9bb0b0521380a1
andreeavilcu <andreea.vilcu@student.unitbv.ro>
2025-05-11 21:13:33 +0300

    Added a plugging in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    kotlin("plugin.serialization") version "2.0.21"
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
